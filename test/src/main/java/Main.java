0bb53ab0d155ccbd87c82488edfdc75db3b76e08
freshman-git <68904944+freshman-git@users.noreply.github.com>
2022-04-17 15:46:09 +0800

    change age to  2

--- test/src/main/java/Main.java
+++ test/src/main/java/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
         Student stu = new Student();
-        stu.setAge(1);
+        stu.setAge(2);
         stu.setName("a");
     }
 }
